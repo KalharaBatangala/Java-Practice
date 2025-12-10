2840efff5aff946ef854a98da5672921165ed72b
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-10 11:43:43 +0530

    Scanner Inputs

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-     
-    System.out.println("Hello World!");
+    
+    //System.out.println("Hello World!");
 
+    System.out.println("Enter your name: ");
+    Scanner scanner = new Scanner(System.in);
+
+    // take inputs
+    String item = scanner.nextLine();
+
+    System.out.println("Your input is: "+item);
     }  
 }
 
