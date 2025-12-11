198a3fe785eeb55f8e714cbbd148f35c71f9f5a4
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-11 23:01:06 +0530

    Method overloading

--- Main.java
+++ Main.java
@@ -3,9 +3,22 @@
 public class Main {
     public static void main(String[] args){
      
-    System.out.println("Hello World!");
+    int x = 5;
+    double y = 5.2;
+
+    System.out.println("plus Function with int: "+plusFunction(x));
+    System.out.println("plus Function with double "+plusFunction(y));
 
     }  
+
+
+    static int plusFunction(int x){
+        return ++x;
+    }
+
+    static double plusFunction(double x){
+        return ++x;
+    }
 }
 
 
