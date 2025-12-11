4fad9576c9c179eba30af437f16b40b78faf7123
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-11 22:55:02 +0530

    Recursive functions

--- Main.java
+++ Main.java
@@ -2,10 +2,22 @@
 
 public class Main {
     public static void main(String[] args){
-     
-    System.out.println("Hello World!");
+    
+    int x = 5;
+    System.out.println("Factorial of "+x+" is : "+factorial(x));
 
     }  
+
+
+
+    static int factorial(int number){
+        if(number > 0){
+            return number * factorial(number - 1);
+        }
+        else{
+            return 1;
+        }
+    }
 }
 
 
