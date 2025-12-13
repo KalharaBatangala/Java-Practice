ba9f25b49660f4dbe81ff8ac5fc959692466f945
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-13 11:32:21 +0530

    Formated time

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,11 @@ public class Main {
         // local time
         LocalTime timenow = LocalTime.now();
         System.out.println("Current time is: "+timenow);
+    
+        DateTimeFormatter formatObject = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String formattedtime = timenow.format(formatObject);
+        System.out.println("Formatted time: "+formattedtime);
+    
     }
     
 }
