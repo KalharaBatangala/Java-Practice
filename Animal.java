9ba1357b5bd11e528b3d6bb941ee24a385e0439f
Kalhara Batangala <kalharabatangala@gmail.com>
2025-12-13 09:04:33 +0530

    Interfaces & Implements

--- Animal.java
+++ Animal.java
@@ -1,53 +1,18 @@
-// Abstract class is a class that cannot be
-// created as an object 
-
-abstract class Animal {
-    
-    abstract void MakeSound();       // abstract methods 
-    abstract void Move();            // do not have a body
-
-    //normal method
-    void sleep(){
-        System.out.println("This animal is sleeping...");
-    }
-
-    public static void main(String[] args) {
-        Animal a1 = new Dog();
-        Animal a2 = new Bird();
-        Animal a3 = new Bird();
-
-        Animal[] animals = {a1, a2, a3};
-        
-        for(Animal anim : animals){
-            anim.MakeSound();           // runtime polymorphism
-        }
-    }
-
-}
-
-class Dog extends Animal{
-    @Override
-    void MakeSound(){
-        System.out.println("Bark");
-    }
-
-    @Override
-    void Move(){
-        System.out.println("Dog moves");
-    }
+// An interface is a completely "abstract class" that is used to 
+// group related methods with empty bodies
+interface Animal{
+    public void animalSound();
+    public void run();
 }
 
 
-class Bird extends Animal{
-    @Override
-    void MakeSound(){
-        System.out.println("Bird chirps: Tweet tweet");
+class Pig implements Animal{
+    public void animalSound(){
+        System.out.println("Pig: wee wee");
     }
 
-    @Override
-    void Move(){
-        System.out.println("Birds fly");
+    public void run(){
+        System.out.println("Pig runs");
     }
 }
-
 
